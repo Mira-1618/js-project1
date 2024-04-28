3b8b7377abb39656f9f4567a5a65b7cd991690d8
Miranda Brown <mira.brown1618@gmail.com>
2024-04-28 13:28:44 -0500

    Added Weather Notes to NavBar

--- scripts.js
+++ scripts.js
@@ -19,3 +19,31 @@ carouselButton.addEventListener('click', function () {
         carousel.cycle();
     }
 }) 
+
+async function fetchWeather() {
+    const apiKey = process.env.OPEN_WEATHER_API_KEY; 
+    const city = "Dallas";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    try {
+        const response = await fetch(url);
+        const data = await response.json(); 
+        console.log(data);
+        displayWeather(data); 
+    } catch (error) {
+        console.error("There was an error!", error); 
+    }
+}
+
+fetchWeather();
+
+function displayWeather(object) {
+    const img = document.createElement('img');
+    img.src = `https://openweathermap.org/img/w/${object.weather[0].icon}.png`;
+    document.getElementById("weather-icon").appendChild(img);
+    document.getElementById("weather-temp").textContent = `${object.main.temp}\u00B0`
+    document.getElementById("weather-description").textContent = `${object.weather[0].description}`
+}
+
+//desc: weather[0].description
+//icons: weather[0].icon
+//temp: main.temp
